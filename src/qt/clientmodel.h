0811c400c8ae131d1bb65ed2f0ccc13ed6cb3b98
kac- <6b6163@gmail.com>
2014-03-25 09:56:38 +0100

    cherry-pick 41c6b8abc60f90e73ae4ad89b0a922638a61bbea (add client startup time) w/ conflicts
    add client startup time as an entry to debug.log (note: logged time in debug.log differs by a few seconds from the one displayed in the Debug window) / make ClientModel::formatClientStartupTime() return a QString
    Author:    Philip Kaufmann <phil.kaufmann@t-online.de>
    
    Conflicts:
    	src/init.cpp
    	src/qt/clientmodel.cpp
    	src/qt/clientmodel.h
    	src/qt/rpcconsole.cpp

--- src/qt/clientmodel.h
+++ src/qt/clientmodel.h
@@ -39,6 +39,7 @@ public:
     QString formatFullVersion() const;
     QString formatBuildDate() const;
     QString clientName() const;
+    QString formatClientStartupTime() const;
 
 private:
     OptionsModel *optionsModel;
