aeec35671802dbf75711d22037d3adcf5aea7d32
kac- <6b6163@gmail.com>
2014-05-09 11:39:24 +0200

    "time" in TxToJSON [getrawtransaction]

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -171,6 +171,7 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, Object& entry)
 {
     entry.push_back(Pair("txid", tx.GetHash().GetHex()));
     entry.push_back(Pair("version", tx.nVersion));
+    entry.push_back(Pair("time", (int)tx.nTime));
     entry.push_back(Pair("locktime", (boost::int64_t)tx.nLockTime));
     Array vin;
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
